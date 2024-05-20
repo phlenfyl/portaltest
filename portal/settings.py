88fa3fb72c6f65334e5dd994da1907bd6345cd0a
phlenfyl <phlenfytest28@gmail.com>
2024-05-20 19:54:21 +0100

    testing

--- portal/settings.py
+++ portal/settings.py
@@ -46,7 +46,7 @@ DEVELOPMENT_MODE = True
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -211,42 +211,42 @@ LOGOUT_REDIRECT_URL = "login"
 WSGI_APPLICATION = 'portal.wsgi.app'
 
 
-JAZZMIN_SETTINGS = {
-    # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "357 Admin Portal",
+# JAZZMIN_SETTINGS = {
+#     # title of the window (Will default to current_admin_site.site_title if absent or None)
+#     "site_title": "357 Admin Portal",
 
-    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "357 Admin Portal",
+#     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+#     "site_header": "357 Admin Portal",
 
-    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "357company Portal",
+#     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+#     "site_brand": "357company Portal",
 
-    # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "assets/admin.png",
+#     # Logo to use for your site, must be present in static files, used for brand on top left
+#     "site_logo": "assets/admin.png",
 
-    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": None,
+#     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+#     "login_logo": None,
 
-    # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": None,
+#     # Logo to use for login form in dark themes (defaults to login_logo)
+#     "login_logo_dark": None,
 
-    # CSS classes that are applied to the logo above
-    "site_logo_classes": "img-circle",
+#     # CSS classes that are applied to the logo above
+#     "site_logo_classes": "img-circle",
 
-    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon": None,
+#     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+#     "site_icon": None,
 
-    # Welcome text on the login screen
-    "welcome_sign": "Welcome to the 357 Admin Portal",
+#     # Welcome text on the login screen
+#     "welcome_sign": "Welcome to the 357 Admin Portal",
 
-    # Copyright on the footer
-    "copyright": "Acme 357company Ltd",
+#     # Copyright on the footer
+#     "copyright": "Acme 357company Ltd",
 
-    # The model admin to search from the search bar, search bar omitted if excluded
-    "search_model": "auth.User",
+#     # The model admin to search from the search bar, search bar omitted if excluded
+#     "search_model": "auth.User",
 
-    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
-    "user_avatar": 'image',
-}
+#     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+#     "user_avatar": 'image',
+# }
 
 
